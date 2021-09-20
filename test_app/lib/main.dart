03fb3ef53bab72ae86eda4bbf38df45276c8d93b
Ruhul Amin <raridoy4@gmail.com>
2021-09-20 12:59:42 +0600

    test widget

---
+++ test_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Home Page Test"),
+            centerTitle: true,
+            leading: Icon(Icons.message),
+            actions: [
+              IconButton(icon: const Icon(Icons.add_a_photo), onPressed: () {},)
+            ],
+          ),
+          body: Center(
+            child: Text("This is my first app" , style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+          )
+        ));
+  }
+}
