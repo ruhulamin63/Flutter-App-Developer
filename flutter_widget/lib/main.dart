0f706d9887f036846ced1abddb4a6c5ee357d7a4
Ruhul Amin <raridoy4@gmail.com>
2021-09-20 14:12:27 +0600

    container + decoration

--- flutter_widget/lib/main.dart
+++ flutter_widget/lib/main.dart
@@ -6,22 +6,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SafeArea(
-          child: Scaffold(
-            appBar: AppBar(
-              title: const Text("Home Page Test"),
-              centerTitle: true,
-              leading: const Icon(Icons.message),
-              actions: [
-                IconButton(icon: const Icon(Icons.add_a_photo), onPressed: () {})
-              ],
-            ),
-            body: const Text(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Home Page Test"),
+            centerTitle: true,
+            leading: const Icon(Icons.message),
+            actions: [
+              IconButton(icon: const Icon(Icons.add_a_photo), onPressed: () {})
+            ],
+          ),
+          body: Container(
+            height: 200,
+            width: 300,
+           decoration: const BoxDecoration(color: Colors.amber),
+
+            child: const Text(
               "This is my first app",
               style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             ),
-          ),
-        ));
+          )
+        ),
+      )
+    );
   }
 }
