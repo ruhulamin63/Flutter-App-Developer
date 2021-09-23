88c84808ef59fa2847d63f7306a8294c8207f193
Ruhul Amin <raridoy4@gmail.com>
2021-09-23 22:40:32 +0600

    PageView like multiple pages

--- flutter_widget/lib/main.dart
+++ flutter_widget/lib/main.dart
@@ -1,45 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_widget/page1.dart';
+import 'package:flutter_widget/page2.dart';
+import 'package:flutter_widget/page3.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: HomePage());
-  }
+  _State createState() => _State();
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class _State extends State<MyApp> {
+  final PageController _controller = PageController(initialPage: 0);
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    var ScreenHeight = MediaQuery.of(context).size.height / 3;
-    var ScreenWidth = MediaQuery.of(context).size.width / 3;
-
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-         body: Column(
-           children: [
-             Expanded(
-               flex: 1,
-                 child: Container(color: Colors.red,)
-             ),
-             Expanded(
-               flex: 1,
-                 child: Container(color: Colors.green,)
-             ),
-             Expanded(
-               flex: 1,
-                 child: Container(color: Colors.yellow,)
-             ),
-           ],
-         ),
-        )
+      home: Scaffold(
+        body: PageView(
+          scrollDirection: Axis.vertical,
+          controller: _controller,
+          children: const [
+            page1(),
+            page2(),
+            page3(),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ flutter_widget/lib/page2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class page2 extends StatelessWidget {
+  const page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blueAccent,
+    );
+  }
+}
