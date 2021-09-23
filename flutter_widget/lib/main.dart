d555ea933d75adab2fe0f23cc7cc88ca769e6712
Ruhul Amin <raridoy4@gmail.com>
2021-09-23 22:07:11 +0600

    Expanded widget

--- flutter_widget/lib/main.dart
+++ flutter_widget/lib/main.dart
@@ -7,11 +7,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: HomePage()
-    );
+        debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
@@ -20,44 +17,29 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     var ScreenHeight = MediaQuery.of(context).size.height / 3;
-    var ScreenWidth = MediaQuery.of(context).size.width /3 ;
+    var ScreenWidth = MediaQuery.of(context).size.width / 3;
 
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GridView.count(
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-            crossAxisCount: 2,
-            children: [
-              Container(
-                height: ScreenHeight,
-                width: ScreenWidth,
-                color: Colors.amber,
-              ),
-              Container(
-                height: ScreenHeight,
-                width: ScreenWidth,
-                color: Colors.amber,
-              ),
-              Container(
-                height: ScreenHeight,
-                width: ScreenWidth,
-                color: Colors.amber,
-              ),
-              Container(
-                height: ScreenHeight,
-                width: ScreenWidth,
-                color: Colors.amber,
-              )
-            ],
-          ),
-        ),
-      )
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+         body: Column(
+           children: [
+             Expanded(
+               flex: 1,
+                 child: Container(color: Colors.red,)
+             ),
+             Expanded(
+               flex: 1,
+                 child: Container(color: Colors.green,)
+             ),
+             Expanded(
+               flex: 1,
+                 child: Container(color: Colors.yellow,)
+             ),
+           ],
+         ),
+        )
     );
   }
 }
