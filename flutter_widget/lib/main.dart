ce3a09cc1518d6970a0c7cb1345876550b4f0b0a
Ruhul Amin <raridoy4@gmail.com>
2021-09-23 14:01:23 +0600

    create card

--- flutter_widget/lib/main.dart
+++ flutter_widget/lib/main.dart
@@ -7,18 +7,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final pages = [
-      Container(color: Colors.amber,),
-      Container(color: Colors.blue,),
-      Container(color: Colors.black),
-      Container(color: Colors.amber,),
-    ];
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    var ScreenHeight = MediaQuery.of(context).size.height / 3;
+    var ScreenWidth = MediaQuery.of(context).size.width /3 ;
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: LiquidSwipe(
-          pages: pages,
+        body: Center(
+          child: Card(
+            elevation: 20,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20)),
+            ),
+
+            child: Container(
+              height:  ScreenHeight,
+              width: ScreenWidth,
+            ),
+          ),
         ),
       )
     );
