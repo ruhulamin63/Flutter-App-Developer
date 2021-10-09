b307aa4c9c2623b9470a9b3285b492298021bbd4
Ruhul Amin <raridoy4@gmail.com>
2021-10-09 22:42:36 +0600

    login page build + front + image using

--- front_family/lib/main.dart
+++ front_family/lib/main.dart
@@ -26,6 +26,8 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff2d3436),
+
       appBar: AppBar(
         title: Text('Home'),
        centerTitle: true,
@@ -33,6 +35,8 @@ class _HomePageState extends State<HomePage> {
 
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
           children: const [
             CircleAvatar(
              backgroundImage: AssetImage('images/diamond.png'),
@@ -42,9 +46,56 @@ class _HomePageState extends State<HomePage> {
             Text(
               'Ruhul Amin',
               style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.yellow,
+                fontFamily: 'Pacifico'
+              ),
+            ),
+            Text(
+              'Flutter Developer',
+              style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-                color: Colors.red,
+                color: Colors.yellow,
+                fontFamily: 'SourceSansPro',
+                letterSpacing: 4
+              ),
+            ),
+            SizedBox(
+              height: 20,width: 250,
+              child: Divider(
+                color: Colors.white,
+              ),
+            ),
+            Card(
+             // margin: EdgeInsets.all(10),
+              //margin: EdgeInsets.only(right: 10),
+              margin: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.message,color: Colors.teal,),
+                title: Text(
+                  'Hello Everyone',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+             // margin: EdgeInsets.all(10),
+              //margin: EdgeInsets.only(right: 10),
+              margin: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.mail,color: Colors.teal,),
+                title: Text(
+                  'raridoy@gmail.com',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.teal,
+                  ),
+                ),
               ),
             ),
           ],
